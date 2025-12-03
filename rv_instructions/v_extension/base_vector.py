5a6a0349d95cee7466c2ea4657d5370afedff6e3
gundamdouble00 <nguyenngocminhquocp@gmail.com>
2025-12-03 19:35:32 +0700

    change lmul, sew to number; change ConfigurationSetting

--- rv_instructions/v_extension/base_vector.py
+++ rv_instructions/v_extension/base_vector.py
@@ -5,12 +5,15 @@ from rv_instructions.base_instruction import BaseInstruction
 from rv_types.registers import ACTIVE_REG
 
 
-def vector_reg(lmul: int) -> int:
-    return random.randrange(0, 32, lmul)
+def vector_reg(lmul: float) -> int:
+    step: int = int(lmul)
+    if step < 1:
+        step = 1
+    return random.randrange(start=0, stop=32, step=step)
 
 
 class BaseVectorIns(BaseInstruction, ABC):
-    def __init__(self, name: str, index: int, lmul: int, sew: int) -> None:
+    def __init__(self, name: str, index: int, lmul: float, sew: int) -> None:
         super().__init__(name, index)
         self.lmul = lmul
         self.sew = sew
@@ -20,8 +23,82 @@ class BaseVectorIns(BaseInstruction, ABC):
         pass
 
 
+v_sew: list[int] = [8, 16, 32]
+v_lmul: list[float] = [1 / 8, 1 / 4, 1 / 2, 1, 2, 4, 8]
+v_tail: list[str] = ["ta", "tu"]
+v_mask: list[str] = ["ma", "mu"]
+
+
+class ConfigurationSetting(BaseVectorIns):
+    def __init__(self, name: str, index: int, lmul: float, sew: int) -> None:
+        # vsetvl t0, a3, x0
+        # vsetvli t0, a3, e32, m1, ta, ma
+        # vsetivli t0, 12, e32, m1, ta, ma
+
+        super().__init__(name, index, lmul, sew)
+        self.des = ""
+        self.src1 = ""
+        self.src2 = ""
+        self.sew = int(0)
+        self.lmul = float(0.0)
+        self.tail = ""
+        self.mask = ""
+
+        self.des = random.choice(ACTIVE_REG)
+        if len(name) <= 7:
+            # vsetvl, vsetvli
+            self.src1 = random.choice(ACTIVE_REG)
+        else:
+            # vsetivli
+            self.src1 = f"{random.randint(0, 31)}"
+
+        if self.name == "vsetvl":
+            self.src2 = "x0"
+            return
+
+        vtype: list[str] = []
+        while True:
+            # LMUL >= SEW/ELEN
+            self.sew = random.choice(v_sew)
+            self.lmul = random.choice(v_lmul)
+            if self.lmul * 32 >= float(self.sew):
+                break
+
+        self.tail = random.choice(v_tail)
+        self.mask = random.choice(v_mask)
+
+        vtype.append(f"{self.sew}")
+        vtype.append(f"{self.lmul}")
+        vtype.append(self.tail)
+        vtype.append(self.mask)
+        self.src2 = vtype
+
+    def generate(self) -> str:
+        ins = f"{self.name} {self.des}, {self.src1}, "
+        for i in range(len(self.src2)):
+            field = self.src2[i]
+            ins += field
+            if i < len(self.src2) - 1:
+                ins += ", "
+
+        return ins
+
+
+class LoadsStores(BaseVectorIns):
+    def __init__(self, name: str, index: int, lmul: float, sew: int) -> None:
+        super().__init__(name, index, lmul, sew)
+
+        self.des = ""
+        self.src2 = ""
+        self.src1 = ""
+        self.mask = random.choice([True, False])
+
+    def generate(self) -> str:
+        return ""
+
+
 class IntegerArithmeticIns(BaseVectorIns):
-    def __init__(self, name: str, index: int, lmul: int, sew: int) -> None:
+    def __init__(self, name: str, index: int, lmul: float, sew: int) -> None:
         super().__init__(name, index, lmul, sew)
 
         self.des = f"v{vector_reg(lmul)}"
@@ -34,66 +111,4 @@ class IntegerArithmeticIns(BaseVectorIns):
         if self.mask:
             ins = ", v0.t"
         ins = f"{self.name} {self.des}, {self.src2}, {self.src1}" + ins
-        return ins
-
-
-class LoadsStores(BaseVectorIns):
-    def __init__(self, name: str, index: int, lmul: int, sew: int) -> None:
-        super().__init__(name, index, lmul, sew)
-
-        self.des = ""
-        self.src2 = ""
-        self.src1 = ""
-        self.mask = random.choice([True, False])
-
-    def generate(self) -> str:
-        return ""
-
-
-v_sew: list[str] = ["e8", "e16", "e32"]
-v_lmul: list[str] = ["mf8", "mf4", "mf2", "m1", "m2", "m4", "m8"]
-v_tail: list[str] = ["ta", "tu"]
-v_mask: list[str] = ["ma", "mu"]
-
-
-class ConfigurationSetting(BaseVectorIns):
-    def __init__(self, name: str, index: int, lmul: int, sew: int) -> None:
-        # vsetvl t0, a3, x0
-        # vsetvli t0, a3, e32, m1, ta, ma
-        # vsetivli t0, 12, e32, m1, ta, ma
-
-        super().__init__(name, index, lmul, sew)
-
-        self.des = f"{random.choice(ACTIVE_REG)}"
-        if len(name) <= 7:
-            # vsetvl, vsetvli
-            self.src1 = f"{random.choice(ACTIVE_REG)}"
-        else:
-            # vsetivli
-            self.src1 = f"{random.randint(0, 31)}"
-
-        vtype: list[str] = []
-        sew = random.choice(v_sew)
-        lmul = random.choice(v_lmul)
-        tail = random.choice(v_tail)
-        mask = random.choice(v_mask)
-
-        if len(name) == 6:
-            # name == vsetvl
-            vtype.append("x0")
-        else:
-            vtype.append(sew)
-            vtype.append(lmul)
-            vtype.append(tail)
-            vtype.append(mask)
-        self.src2 = vtype
-
-    def generate(self) -> str:
-        ins = f"{self.name} {self.des}, {self.src1}, "
-        for i in range(len(self.src2)):
-            field = self.src2[i]
-            ins += field
-            if i < len(self.src2) - 1:
-                ins += ", "
-
         return ins
