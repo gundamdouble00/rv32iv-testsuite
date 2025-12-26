026a3a5341ab27b3445b457171629b7c7a236b72
gundamdouble00 <nguyenngocminhquocp@gmail.com>
2025-12-26 21:57:00 +0700

    add self.type to all integer classes

--- rv_instructions/integer/b_type.py
+++ rv_instructions/integer/b_type.py
@@ -2,7 +2,7 @@ import random
 
 from rv_instructions.integer.base_integer import BaseIntegerIns
 from rv_types.registers import ACTIVE_REG
-from rv_types.riscv_infor import type_of_ins
+from rv_types.instruction_type import type_of_ins
 
 
 class BTypeIns(BaseIntegerIns):
@@ -10,8 +10,9 @@ class BTypeIns(BaseIntegerIns):
         # name rs1, rs2, offset/label
         super().__init__(name, index)
 
-        self.src1 = f"{random.choice(ACTIVE_REG)}"
-        self.src2 = f"{random.choice(ACTIVE_REG)}"
+        self.src1 = random.choice(ACTIVE_REG)
+        self.src2 = random.choice(ACTIVE_REG)
+        self.src3 = "end"
         class_name: str = BTypeIns.__name__
         self.type = type_of_ins[class_name]
 
--- rv_instructions/integer/j_type.py
+++ rv_instructions/integer/j_type.py
@@ -1,8 +1,5 @@
-import random
-
 from rv_instructions.integer.base_integer import BaseIntegerIns
-from config import PROGRAM_LEN
-from rv_types.riscv_infor import type_of_ins
+from rv_types.instruction_type import type_of_ins
 
 
 class JTypeIns(BaseIntegerIns):
@@ -10,8 +7,9 @@ class JTypeIns(BaseIntegerIns):
         super().__init__(name, index)
 
         # jal extra_func
+        self.des = "x12"
         class_name: str = JTypeIns.__name__
         self.type = type_of_ins[class_name]
 
     def generate(self) -> str:
-        return f"{self.name} extra_func"
+        return f"{self.name} {self.des}, extra_func"
